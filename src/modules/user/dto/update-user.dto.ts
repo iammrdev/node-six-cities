75761a93c212e29e9a64ffac3863088e95a037bc
Vitalii Iashchuk <iashchuk@hotmail.com>
2022-10-10 22:19:06 +0300

    M5T2: update user module

---
+++ src/modules/user/dto/update-user.dto.ts
@@ -0,0 +1,7 @@
+export default class UpdateUserDto {
+
+  public firstname?: string;
+  public lastname?: string;
+  public avatar?: string;
+  public type?: 'base' | 'pro';
+}
--- src/modules/user/user.interface.ts
+++ src/modules/user/user.interface.ts
@@ -1,5 +1,6 @@
 import { DocumentType } from '@typegoose/typegoose';
 import CreateUserDto from './dto/create-user.dto.js';
+import UpdateUserDto from './dto/update-user.dto.js';
 import { UserEntity } from './user.entity.js';
 
 export interface UserServiceInterface {
@@ -8,4 +9,6 @@ export interface UserServiceInterface {
   findByEmail(email: string): Promise<DocumentType<UserEntity> | null>;
 
   findOrCreate(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>>;
+
+  updateById(userId: string, dto: UpdateUserDto): Promise<DocumentType<UserEntity> | null>;
 }
