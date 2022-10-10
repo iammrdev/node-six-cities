b2ee2ff0e8a85aa8e1a7f8c7d128e541b449135a
Vitalii Iashchuk <iashchuk@hotmail.com>
2022-10-10 22:17:29 +0300

    M5T2: add comment module

---
+++ src/modules/comment/comment.interface.ts
@@ -0,0 +1,11 @@
+import { DocumentType } from '@typegoose/typegoose';
+import { CommentEntity } from './comment.entity.js';
+import CreateCommentDto from './dto/create-comment.dto.js';
+
+export interface CommentServiceInterface {
+  create(dto: CreateCommentDto): Promise<DocumentType<CommentEntity>>;
+
+  findByOfferId(offerId: string): Promise<DocumentType<CommentEntity>[]>;
+
+  deleteByOfferId(offerId: string): Promise<number | null>;
+}
