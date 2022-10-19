2c4d0eb62138ea36500b2c7a7b37a8d08a222ee1
Vitalii Iashchuk <iashchuk@hotmail.com>
2022-10-19 22:28:51 +0300

    M7T1: add middlewares handler

--- src/types/route.interface.ts
+++ src/types/route.interface.ts
@@ -1,8 +1,10 @@
 import { HttpMethod } from './http.enum.js';
 import { NextFunction, Request, Response } from 'express';
+import { MiddlewareInterface } from '../middlewares/middleware.interface.js';
 
 export interface RouteInterface {
   path: string;
   method: HttpMethod;
   handler: (req: Request, res: Response, next: NextFunction) => void;
+  middlewares?: MiddlewareInterface[];
 }
