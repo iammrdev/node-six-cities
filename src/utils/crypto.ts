8ba1e1c9ce4a533edc21c41b76abb885bdd8c5ed
Vitalii Iashchuk <iashchuk@hotmail.com>
2022-10-22 20:04:23 +0300

    M8T1: add verify user

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,6 +1,14 @@
 import crypto from 'crypto';
+import * as jose from 'jose';
 
 export const createSHA256 = (line: string, salt: string): string => {
   const shaHasher = crypto.createHmac('sha256', salt);
   return shaHasher.update(line).digest('hex');
 };
+
+export const createJWT = async (algoritm: string, jwtSecret: string, payload: object): Promise<string> =>
+  new jose.SignJWT({ ...payload })
+    .setProtectedHeader({ alg: algoritm })
+    .setIssuedAt()
+    .setExpirationTime('2d')
+    .sign(crypto.createSecretKey(jwtSecret, 'utf-8'));
